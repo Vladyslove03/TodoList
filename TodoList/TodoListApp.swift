8e1bb6bb6e46ff2f3b5e2dd57e77c887d6eee81c
Vladyslove03 <nadaxovskiy@gmail.com>
2026-04-03 10:51:07 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by User on 03.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
